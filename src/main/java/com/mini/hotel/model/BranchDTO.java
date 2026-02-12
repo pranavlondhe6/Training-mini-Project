b1d914ffea3f1bc5bd119b4ebf064d33a9dcbd45
Pranav <londhepranav2@gmail.com>
2026-02-12 21:37:43 +0530

    BranchDTO and BranchRepository classes created to exchange and persist data

---
+++ src/main/java/com/mini/hotel/model/BranchDTO.java
@@ -0,0 +1,10 @@
+package com.mini.hotel.model;
+
+import lombok.Data;
+
+@Data
+public class BranchDTO {
+
+    private String branchName;
+    private String branchAddress;
+}
---
+++ src/main/java/com/mini/hotel/repository/BranchRepository.java
@@ -0,0 +1,7 @@
+package com.mini.hotel.repository;
+
+import com.mini.hotel.entity.Branch;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BranchRepository extends JpaRepository<Branch, Long> {
+}
